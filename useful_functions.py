e29ff1560b5c840ec18c14181bb89851708d8ae4
Василенко Иван Геннадьевич <i.vasilenko@rdp.ru>
2024-10-24 16:15:40 +0300

    добавил скачивание apk файла

--- useful_functions.py
+++ useful_functions.py
@@ -31,3 +31,26 @@ def generating_request_with_TCP_payload(ips, port, payload_data, search):
         except Exception as se:
             print(f"{ip} НЕ Прошёл валидацию! ERROR: {se}")
             sock.close()
+
+def get_xapk_using_proxy(URLS, file_name: str) -> None:
+    """
+    Функция для скачивания XAPK файла.
+    :param file_name: Имя приложения
+    :return: None
+    """
+    for url in URLS:
+        logging.info(f"Попытка скачать XAPK c URL: {url}")
+        result = self._request_via_proxy(method="get", url=url) # запрс с прксями
+        if result.status_code == 200:
+            with open(file_name, "wb") as file:
+                for chunk in result.iter_content(chunk_size=1024):
+                    if chunk:
+                        file.write(chunk)
+                logging.info("Файл XAPK скачен")
+                break
+        else:
+            logging.info(f"Не получилось скачать XAPK c URL: {url} ")
+    else:
+        messages = "Файл XAPK не удалось скачать. Работа сборщика остановлена"
+        logging.error(messages)
+        raise Exception(messages)
